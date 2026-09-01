a82fdf9e79bfcdc8bade11b42f1ec15eeb29d96f
John Nazarene <jannazarene09@gmail.com>
2026-09-02 03:21:42 +0800

    Fix jsdom vercel deploy error

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  serverExternalPackages: ['jsdom', '@mozilla/readability'],
 };
 
 export default nextConfig;
